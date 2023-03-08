940ade3f1168fa6c6371ed4c46bf0c5fc960813b
agusdm97 <demarco.agustin.97@gmail.com>
2023-03-08 20:10:58 -0300

    Lógica de subida de archivos

--- app/1_Home.py
+++ app/1_Home.py
@@ -1,14 +1,43 @@
 import streamlit as st
+import pandas as pd
 from PIL import Image
-st.header('Gender Gap Tracker')
+from time import sleep
 
-st.write('''Esta App se enfoca en monitorear la brecha de resultados de la prueba de selección universitaria 
+st.title("Gender Gap Tracker")
+
+st.write(
+    """Esta App se enfoca en monitorear la brecha de resultados de la prueba de selección universitaria 
 entre hombres y mujeres de un establecimiento en particular. Con una interfaz fácil de usar, 
 el Dashboard permite a los usuarios visualizar de manera clara y detallada los resultados de rendimiento
  de ambos géneros en diferentes áreas de estudio. Se ofrece una serie de herramientas de análisis 
  que permiten a los usuarios profundizar en los resultados y contribuir al análisis de posibles causas 
- de la brecha de género.''')
+ de la brecha de género."""
+)
 
 image = Image.open("app/gender.jpg")
-st.write("")
-st.image(image, caption="")
+
+st.image(image, use_column_width="always", caption="")
+
+uploaded_file = st.file_uploader(label="Sube tu archivo aquí", type=["csv"])
+
+if uploaded_file is not None:
+    try:
+        dataframe = pd.read_csv(uploaded_file)
+    except:
+        st.error("Error al cargar el archivo", icon="❌")
+    else:
+        if st.session_state.get("data") is None:
+            st.session_state["data"] = dataframe
+            placeholder = st.empty()
+            placeholder.success("El archivo fue cargado con éxito", icon="✅")
+            sleep(3)
+            placeholder.empty()
+        else:
+            st.session_state["data"] = dataframe
+            placeholder = st.empty()
+            placeholder.warning(
+                "Estas sobrescribiendo los datos anteriores",
+                icon="⚠",
+            )
+            sleep(3)
+            placeholder.empty()
